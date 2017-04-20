2dc738a65a732cd3c39946f6a4d11eb618aa5296
Scorg <gudkovvitek@gmail.com>
2017-04-21 00:05:01 +0500

    улучшена поддержка полной выборки

---
+++ controllers/test.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+const _ = require('lodash');
+const moment = require('moment');
+const error = require(__libdir + '/error.js');
+const page = require(__libdir + '/page.js');
+const models = require(__rootdir + '/models');
+
+exports.setup = function (req, res) {
+	Promise.all(_.map([
+		'user',
+		'user.list',
+		'user.info',
+		'user.set-roles'
+	], name => models.Permission.findOrCreate({
+		where: {name: name },
+		defaults: { name: name }
+	})))
+		.then(perms =>
+			models.Role.findOrCreate({
+				where: {
+					name: 'Администратор'
+				},
+				defaults: {
+					name: 'Администратор',
+					permissions: perms.map(x => x[0])
+				}
+			}))
+		.then(([role, created]) => {
+			return models.User
+				.findById(req.session.user && req.session.user.id || req.params.id)
+				.then(user => {
+					if (user) {
+						user.addRole(role);
+						return user.save();
+					}
+				})
+				.then(u => {
+					res.status(200).json({
+						data: role.id
+					});
+				});
+		})
+		.catch(error.handleInternal(req, res));
+};
---
+++ routes/test.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+const express = require('express');
+const ctl = require(__rootdir + '/controllers/test.js');
+const auth = require(__libdir + '/auth.js');
+
+module.exports = function (root) {
+	const router = express.Router();
+
+	// todo: Экспресс ведь не против, если я обработчики позже указываю?
+	root.use('/test', router);
+	
+	router.post('/setup', ctl.setup);
+};
